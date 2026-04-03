6a82fd63785ee730cf85a9427abbd03d022e923c
Thanh Le <lethanh27112002@gmail.com>
2026-04-03 16:42:32 +0700

    Update dependencies and enhance Chatlist component with improved structure and AddUser integration

--- src/components/list/chatList/Chatlist.jsx
+++ src/components/list/chatList/Chatlist.jsx
@@ -1,48 +1,54 @@
-import { useState } from "react"
-import "./chatList.css"
+import { useState } from "react";
+import "./chatList.css";
+import AddUser from "./addUser/AddUser";
 
 const Chatlist = () => {
-
   const [addMode, SetAddMode] = useState(false);
   return (
     <div className="chatList">
-        <div className="search">
-            <div className="searchBar">
-                <img src="./search.png" alt="search" />
-                <input type="text" placeholder="Search" />
-            </div>
-            <img src={addMode? "./minus.png" : "./plus.png"} alt="plus" className="add" onClick={() => SetAddMode((prev) => !prev)}/>
+      <div className="search">
+        <div className="searchBar">
+          <img src="./search.png" alt="search" />
+          <input type="text" placeholder="Search" />
         </div>
-        <div className="item">
-            <img src="./avatar.png" alt="avatar" />
-            <div className="texts">
-                <h2>John Doe</h2>
-                <p>Hello, how are you?</p>
-            </div>
+        <img
+          src={addMode ? "./minus.png" : "./plus.png"}
+          alt="plus"
+          className="add"
+          onClick={() => SetAddMode((prev) => !prev)}
+        />
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="avatar" />
+        <div className="texts">
+          <h2>John Doe</h2>
+          <p>Hello, how are you?</p>
         </div>
-        <div className="item">
-            <img src="./avatar.png" alt="avatar" />
-            <div className="texts">
-                <h2>John Doe</h2>
-                <p>Hello, how are you?</p>
-            </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="avatar" />
+        <div className="texts">
+          <h2>John Doe</h2>
+          <p>Hello, how are you?</p>
         </div>
-        <div className="item">
-            <img src="./avatar.png" alt="avatar" />
-            <div className="texts">
-                <h2>John Doe</h2>
-                <p>Hello, how are you?</p>
-            </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="avatar" />
+        <div className="texts">
+          <h2>John Doe</h2>
+          <p>Hello, how are you?</p>
         </div>
-        <div className="item">
-            <img src="./avatar.png" alt="avatar" />
-            <div className="texts">
-                <h2>John Doe</h2>
-                <p>Hello, how are you?</p>
-            </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="avatar" />
+        <div className="texts">
+          <h2>John Doe</h2>
+          <p>Hello, how are you?</p>
         </div>
+      </div>
+      {addMode && <AddUser />}
     </div>
-  )
-}
+  );
+};
 
-export default Chatlist
+export default Chatlist;
